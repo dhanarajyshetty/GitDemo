445ddb8eca127f0f3dd90e264a0bb84bfb0d6d93
dhanarajyshetty <dhanarajyshetty01@gmail.com>
2023-08-14 15:44:11 +0530

    Subtraction function

--- python_progm/py1.py
+++ python_progm/py1.py
@@ -3,3 +3,9 @@ def add(n1,n2):
 a=int(input('enter num1:'))
 b=int(input('enter num2:'))
 print('sum=',add(a,b))
+
+def sub(n1,n2):
+    return (n1-n2)
+a=int(input('enter num1:'))
+b=int(input('enter num2:'))
+print('sum=',sub(a,b))
