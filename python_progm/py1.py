6e87df311048f86477fbea9b828c05ba1e44cf2c
dhanarajyshetty <dhanarajyshetty01@gmail.com>
2023-08-16 11:48:00 +0530

    division added to file

--- python_progm/py1.py
+++ python_progm/py1.py
@@ -15,3 +15,10 @@ def multi(n1,n2):
 a=int(input('enter num1:'))
 b=int(input('enter num2:'))
 print('sum=',multi(a,b))
+
+
+def div(n1,n2):
+    return (n1/n2)
+a=int(input('enter num1:'))
+b=int(input('enter num2:'))
+print('sum=',div(a,b))
